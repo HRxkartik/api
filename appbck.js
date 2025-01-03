049d2447df1c11a85fcdb300a1b8f91cdc7f70ec
HRxkartik <kartikarora2311@gmail.com>
2025-01-03 20:15:05 +0530

    appbck.js

--- appbck.js
+++ appbck.js
@@ -2,18 +2,7 @@ const fs=require('fs')
 var http = require('http');
 const std=require('./Model/studentlist')
 http.createServer(function (req, res) {
-    res.write(JSON.stringify(std.student)); //write a response to the client
-    res.end(); //end the response
+    res.write(JSON.stringify(std.student)); 
+    res.end();
 }).listen(8080);
-// fs.appendFile('mynewfile3.txt', ' This is my text.', function (err) {
-//     if (err) throw err;
-//     console.log('Updated!');
-//   });
-// fs.rename('sum.js', 'myrenamedfile.js', function (err) {
-//     if (err) throw err;
-//     console.log('File Renamed!');
-//   });
-// fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
-//     if (err) throw err;
-//     console.log('Saved!');
-//   });
+
