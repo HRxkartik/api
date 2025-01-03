6b1e9137dcd4a959909ee026dae5483f5ac6367d
HRxkartik <kartikarora2311@gmail.com>
2025-01-03 20:26:12 +0530

    Delete appbck.js

--- appbck.js
+++
@@ -1,8 +0,0 @@
-const fs=require('fs')
-var http = require('http');
-const std=require('./Model/studentlist')
-http.createServer(function (req, res) {
-    res.write(JSON.stringify(std.student)); 
-    res.end();
-}).listen(8080);
-
